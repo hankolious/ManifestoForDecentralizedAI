01ad0e8447adaddc18b57b02bdddec80a059bc06
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-10-16 12:50:19 +0200

    Add initial manifesto content

--- src/DeAIManifesto_frontend/src/main.jsx
+++ src/DeAIManifesto_frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import AIManifestoPage from './AIManifestoPage';
+//import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AIManifestoPage />
   </React.StrictMode>,
 );
